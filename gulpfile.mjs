7a02a69b5f53eb7353cb45a87ffe7727fa51b8d3
petriwna <petriwna43@gmail.com>
2024-06-10 19:25:47 +0300

    add header and styling

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -12,10 +12,10 @@ const paths = {
         src: 'src/scss/**/*.scss',
         dest: 'dist/css/'
     },
-    // scripts: {
-    //     src: 'src/js/**/*.js',
-    //     dest: 'dist/js/'
-    // },
+    fonts: {
+        src: 'src/public/fonts/*.woff2',
+        dest: 'dist/public/fonts'
+    },
     images: {
         src: 'src/public/images/**/*.{png,svg,jpg}',
         dest: 'dist/public/images/'
@@ -34,6 +34,11 @@ gulp.task('clean', () => {
 gulp.task('copy:html', () => {
     return gulp.src(paths.html.src)
         .pipe(gulp.dest(paths.html.dest))
+});
+
+gulp.task('copy:fonts', () => {
+    return gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dest))
 });
 
 gulp.task('sass', () => {
@@ -64,12 +69,12 @@ gulp.task('server', () => {
     });
 });
 
-gulp.task('default', gulp.series('copy:html', 'sass', 'images', 'server'));
+gulp.task('default', gulp.series('copy:html', 'copy:fonts', 'sass', 'images', 'server'));
 
 gulp.task('watch', () => {
     gulp.watch(paths.html.src, gulp.series('copy:html'));
+    gulp.watch(paths.html.src, gulp.series('copy:fonts'));
     gulp.watch(paths.styles.src, gulp.series('sass'));
-    // gulp.watch(paths.scripts.src, gulp.series('scripts'));
     gulp.watch(paths.images.src, gulp.series('images'));
     gulp.watch('src/**/*', gulp.series('reload'));
 

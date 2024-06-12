dbdc8b07b2acf8b020bb9f2042b97a99e5bab37e
petriwna <petriwna43@gmail.com>
2024-06-12 22:58:30 +0300

    fix views

--- src/js/ViewToggle.js
+++ src/js/ViewToggle.js
@@ -2,15 +2,15 @@ export class ViewToggle {
     constructor() {
         this.listButton = document.querySelector('.row');
         this.gridButton = document.querySelector('.grid');
-        this.listView = document.querySelector('.content-section__list-row');
-        this.gridView = document.querySelector('.content-section__list-grid');
+        this.container = document.querySelector('.content-section__list');
+        this.items = document.querySelectorAll('.item');
 
         this.initView();
         this.addEventListeners();
     }
 
     initView() {
-        this.gridView.classList.add('visually-hidden');
+        this.container.classList.add('list-row');
         this.listButton.classList.add('icon-btn--active');
     }
 
@@ -20,16 +20,18 @@ export class ViewToggle {
     }
 
     showListView() {
-        this.listView.classList.remove('visually-hidden');
-        this.gridView.classList.add('visually-hidden');
+        this.container.classList.remove('list-grid');
+        this.container.classList.add('list-row');
         this.gridButton.classList.remove('icon-btn--active');
         this.listButton.classList.add('icon-btn--active');
+        this.items[this.items.length - 1].style.display = 'flex';
     }
 
     showGridView() {
-        this.gridView.classList.remove('visually-hidden');
-        this.listView.classList.add('visually-hidden');
+        this.container.classList.remove('list-row');
+        this.container.classList.add('list-grid');
         this.listButton.classList.remove('icon-btn--active');
         this.gridButton.classList.add('icon-btn--active');
+        this.items[this.items.length - 1].style.display = 'none';
     }
 }

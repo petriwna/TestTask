41b38154a7d386f947daa10f823ea606623240c7
petriwna <petriwna43@gmail.com>
2024-06-13 11:29:39 +0300

    add click Load more button

--- src/js/ViewToggle.js
+++ src/js/ViewToggle.js
@@ -2,8 +2,11 @@ export class ViewToggle {
     constructor() {
         this.listButton = document.querySelector('.row');
         this.gridButton = document.querySelector('.grid');
+        this.moreButton = document.querySelector('.more');
         this.container = document.querySelector('.content-section__list');
         this.items = document.querySelectorAll('.item');
+
+        this.itemLength = 0;
 
         this.initView();
         this.addEventListeners();
@@ -12,11 +15,16 @@ export class ViewToggle {
     initView() {
         this.container.classList.add('list-row');
         this.listButton.classList.add('icon-btn--active');
+
+        for(let i = 0; i < 9; i++) {
+            this.items[i].style.display = 'flex';
+        }
     }
 
     addEventListeners() {
         this.listButton.addEventListener('click', () => this.showListView());
         this.gridButton.addEventListener('click', () => this.showGridView());
+        this.moreButton.addEventListener('click', () => this.showMoreItems());
     }
 
     showListView() {
@@ -24,7 +32,13 @@ export class ViewToggle {
         this.container.classList.add('list-row');
         this.gridButton.classList.remove('icon-btn--active');
         this.listButton.classList.add('icon-btn--active');
-        this.items[this.items.length - 1].style.display = 'flex';
+
+        this.items.forEach((item) => {
+            item.style.display = 'none';
+        });
+
+
+        this.showInitialItems();
     }
 
     showGridView() {
@@ -32,6 +46,41 @@ export class ViewToggle {
         this.container.classList.add('list-grid');
         this.listButton.classList.remove('icon-btn--active');
         this.gridButton.classList.add('icon-btn--active');
-        this.items[this.items.length - 1].style.display = 'none';
+
+        this.items.forEach((item) => {
+            item.style.display = 'none';
+        })
+
+        this.showInitialItems();
+    }
+
+    showInitialItems() {
+        const initialDisplayCount = this.container.classList.contains('list-row') ? 9 : 8;
+        const displayStyle = this.container.classList.contains('list-row') ? 'flex' : 'block';
+
+        for (let i = 0; i < initialDisplayCount && i < this.items.length; i++) {
+            this.items[i].style.display = displayStyle;
+        }
+
+        this.itemLength = initialDisplayCount;
+    }
+
+    showMoreItems() {
+        const additionalItemCount = 4;
+        const displayStyle = this.container.classList.contains('list-row') ? 'flex' : 'block';
+
+        let displayedItems = 0;
+
+        this.items.forEach((item) => {
+            if (item.style.display === displayStyle) {
+                displayedItems++;
+            }
+        });
+
+        for (let i = displayedItems; i < displayedItems + additionalItemCount && i < this.items.length; i++) {
+            this.items[i].style.display = displayStyle;
+        }
+
+        this.itemLength += additionalItemCount;
     }
 }
